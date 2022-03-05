ff5cb8e68accad75b699a7978891b77a4edf9247
Najmul H. Bappy <http.bappy@gmail.com>
2022-03-05 21:15:36 +0600

    Added Name and ID and Button

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
-// Input
 import React, { useState } from "react";
-import { Text, TextInput, View } from "react-native";
-const PizzaTranslator = () => {
-  const [text, setText] = useState("");
-  return (
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+  TextInput,
+} from "react-native";
+
+const Separator = () => <View style={styles.separator} />;
+
+const App = () => (
+  <SafeAreaView style={styles.container}>
     <View style={{ padding: 10 }}>
       <Text style={{ padding: 10, fontSize: 15 }}>Enter Your Name:</Text>
       <TextInput
@@ -14,10 +23,8 @@ const PizzaTranslator = () => {
           backgroundColor: "#efefef",
         }}
         placeholder="Najmul Hossain"
-        onChangeText={(text) => setText(text)}
-        defaultValue={text}
       />
-      <Text style={{ padding: 10, fontSize: 15 }}>Enter Your ID:</Text>
+      <Text style={{ padding: 10, fontSize: 15 }}>Enter Your Roll/ID:</Text>
       <TextInput
         style={{
           padding: 10,
@@ -26,10 +33,34 @@ const PizzaTranslator = () => {
           backgroundColor: "#efefef",
         }}
         placeholder="CSE1802014027"
-        onChangeText={(text) => setText(text)}
-        defaultValue={text}
       />
     </View>
-  );
-};
-export default PizzaTranslator;
+    <View>
+      <Button title="Submit" />
+    </View>
+    <Separator />
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: "center",
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: "#737373",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
+
+export default App;
